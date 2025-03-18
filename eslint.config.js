2d0eca7f2568954c83506f9fd03633de53202234
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2025-03-18 21:32:09 +0200

    add lint and pritieer

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,22 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import prettierConfig from './prettier.config.js';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
+    settings: {
+      'import/resolver': {
+        alias: {
+          map: [['@core', './src']],
+          extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        },
+      },
+    },
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
@@ -16,6 +26,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: eslintPluginPrettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +34,8 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/explicit-function-return-type': ['error'],
+      'prettier/prettier': ['error', prettierConfig],
     },
   },
-)
+);
