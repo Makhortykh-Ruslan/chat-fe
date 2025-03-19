5cf8505434c296dc737d653b94075488989b233e
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2025-03-19 21:20:25 +0200

    setting vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
+  plugins: [react(), eslint()],
   server: {
     port: 3000,
     open: true,
   },
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@core': '/src',
+    },
+  },
+  build: {
+    sourcemap: true,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 });
