b274e08f62e0d58ed92799711a0fe468e4fd5aa1
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2025-07-23 23:08:40 +0300

    add logOut func

--- src/app/pages/main/pages/Settings/Settings.page.tsx
+++ src/app/pages/main/pages/Settings/Settings.page.tsx
@@ -1,11 +1,30 @@
 import { ToggleTheme } from '@core/components/ToggleTheme/ToggleTheme.tsx';
+import { useStore } from '@core/store/useStore.tsx';
+import { Button } from '@mui/material';
 import React from 'react';
 
 const SettingsPage: React.FC = () => {
+  const logout = useStore((state) => state.logout);
+
+  const logOut = (): void => {
+    logout();
+    localStorage.clear();
+    sessionStorage.clear();
+
+    window.location.reload();
+  };
+
   return (
     <div>
       <ToggleTheme></ToggleTheme>
       Settings
+      <Button
+        variant='text'
+        onClick={() => logOut()}
+        sx={{ textTransform: 'none', padding: 0, minWidth: 'auto' }}
+      >
+        Log out
+      </Button>
     </div>
   );
 };
